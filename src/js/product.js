68f85a6ba8e8fdb863af54d74982bbbbcda02643
bneveret <eve20003@byui.edu>
2024-11-08 14:43:46 -0700

    WE-individual1 add item to cart fix

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,13 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  var productList = getLocalStorage("so-cart");
+  if (productList === undefined || productList === null) productList = [];
+  productList.push(product);
+  setLocalStorage("so-cart", productList);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
