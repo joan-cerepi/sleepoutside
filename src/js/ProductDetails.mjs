e3b1b719a872d17db113bfb8092fe59ea8c6e083
Beduman <behunz77@gmail.com>
2024-11-16 17:41:38 -0600

    .

---
+++ src/js/ProductDetails.mjs
@@ -0,0 +1,12 @@
+
+
+import ProductData from './ProductData.mjs';
+import ProductDetails from './ProductDetails.mjs';
+import { setLocalStorage, getParam } from './utils.mjs';
+
+const productId = getParam('product');
+const dataSource = new ProductData('tents');
+
+const product = new ProductDetails(productId, dataSource);
+product.init();
+
