33040749223f9297e7f3e1cabd0ce68635766aab
gawahlb <wah23001@byui.edu>
2024-11-15 17:02:38 -0700

    almost done with week 2 individual activity

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -29,3 +29,12 @@ export function setClick(selector, callback) {
   });
   qs(selector).addEventListener("click", callback);
 }
+
+export function renderListWithTemplate(templateFn, parentElement, list, position = "afterbegin", clear = false) {
+  const htmlStrings = list.map(templateFn);
+
+  if(clear) {
+    parentElement.innerHTML = "";
+  }
+    parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+}
